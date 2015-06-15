a34dd2d5e7abb17b8a88f56a4b838ac23462853a
Roger Herikstad <roger.herikstad@gmail.com>
2015-06-15 09:09:39 +0800

    Added more tests for get_intervals

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,6 +6,10 @@ function test_sigint()
 	X[21:30] = 1.0
 	intervals = PeakFinder.get_intervals(X,0.0, 10)
 	@test intervals == {21 => 10}
+	fill!(X, 0.0)
+	X[41:50] = 1.0
+	intervals = PeakFinder.get_intervals(X,0.0, 10)
+	@test intervals == {41 => 10}
 	println("sigint test passed")
 end
 
