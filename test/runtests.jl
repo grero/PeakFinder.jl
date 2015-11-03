6ca5f4c607cf59cc2c7774f0dca6d3e4f3af5729
Roger Herikstad <roger.herikstad@gmail.com>
2015-11-03 11:05:24 +0800

    Fixed 0.4 deprecation warnings for tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -24,17 +24,17 @@ function test_sigint()
 	X = zeros(50)
 	X[21:30] = 1.0
 	intervals = PeakFinder.get_intervals(X,0.0, 10)
-	@test intervals == {21 => 10}
+	@test intervals == Dict((21 => 10))
 	fill!(X, 0.0)
 	X[41:50] = 1.0
 	intervals = PeakFinder.get_intervals(X,0.0, 10)
-	@test intervals == {41 => 10}
+	@test intervals == Dict((41 => 10))
 	println("Sigint test passed")
 end
 
 function test_peaks()
 	X = zeros(50)
-	x = [-5:4]
+	x = collect(-5:4)
 	X[21:30] = exp(-x.^2/2) #gaussian peak
 	peaks = PeakFinder.get_peaks(X, 0.0, 5)
 	@test peaks[1] == PeakFinder.Peak(21, 10, 1.0, 2.506624530883954, 26.0)
